0809f054837d389365bb4800a58609a3b2d79ea3
hiogawa <hi.ogawa.zz@gmail.com>
2016-06-24 02:06:00 +0900

    add mathjax

--- header.php
+++ header.php
@@ -66,6 +66,12 @@
 	wp_head();
 ?>
 <script src="https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js"></script>
+<script type="text/x-mathjax-config">
+  MathJax.Hub.Config({
+    tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}
+  });
+</script>
+<script type="text/javascript" src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
 </head>
 
 <body <?php body_class(); ?>>
